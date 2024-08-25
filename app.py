9b792a190e7a962eabdeb5887af97a4cabda820c
broadsword0007D <99869338+broadsword0007D@users.noreply.github.com>
2024-08-25 19:44:30 +0000

    updated app.py

--- app.py
+++ app.py
@@ -1,38 +1,18 @@
 from flask import Flask, render_template, jsonify
-#idk
-app = Flask(__name__)
+from database import load_jobs
 
-JOBS = [{
-    "id": 1,
-    "title": "Software Engineer",
-    "company": "ABC Corporation",
-    "location": "New York",
-}, {
-    "id": 2,
-    "title": "Data Scientist",
-    "company": "XYZ Inc.",
-    "location": "San Francisco",
-}, {
-    "id": 3,
-    "title": "Marketing",
-    "company": "JHG Corporation",
-    "location": "DELHI",
-}, {
-    "id": 4,
-    "title": "Sales",
-    "company": "DEF Corporation",
-    "location": "Boston",
-}]
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', jobs=JOBS, company_name='MyCaptain')
+    jobs = load_jobs()
+    return render_template('index.html', jobs=jobs, company_name='MyCaptain')
 
 
 @app.route('/api/jobs')
 def jobs():
-    return jsonify(JOBS)
+    return jsonify(load_jobs())
 
 
 if __name__ == "__main__":
