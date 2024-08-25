f3536c2968a94afd887c261a069d173af36e287e
broadsword0007D <99869338+broadsword0007D@users.noreply.github.com>
2024-08-25 19:57:31 +0000

    updated database.py

--- database.py
+++ database.py
@@ -1,13 +1,16 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://avnadmin:AVNS_Pt-JwTOsyw48zx7sT2V@mysql-findmyjob-findmyjob.h.aivencloud.com:15865/defaultdb?charset=utf8mb4")
+engine = create_engine(
+    "mysql+pymysql://avnadmin:AVNS_Pt-JwTOsyw48zx7sT2V@mysql-findmyjob-findmyjob.h.aivencloud.com:15865/defaultdb?charset=utf8mb4"
+)
 
-def load_jobs(): 
+
+def load_jobs():
     with engine.connect() as conn:
-        result = conn.execution_options(stream_results=True).execute(text("select * from jobs"))
-        jobs=[]
+        result = conn.execution_options(stream_results=True).execute(
+            text("select * from jobs"))
+        jobs = []
         rows = result.fetchall()  # Use fetchall() to retrieve all results
         for row in rows:
-           jobs.append(dict(row))
+            jobs.append(dict(row._mapping))
     return jobs
-    
